49fd86fd42d26c36965da7c8ddd186a52c7aa3f6
Zuhra M <zm544052@gmail.com>
2024-02-23 01:50:19 +0500

    Закончила 4 секцию, доделала третью

--- js/script.js
+++ js/script.js
@@ -8,19 +8,19 @@ function changeImage() {
 
 
 // Функция для открывающегося списка
-const accordContainer = document.querySelector('.accordeon-container');
-const question = document.querySelector('.accordeon-question');
+const accordContainer = document.querySelector('.about-container');
+const question = document.querySelector('.about-question');
 accordContainer.addEventListener('click', toggleVisible);
 
 function toggleVisible(evt) {
     console.dir(evt.target);
     // В зависимости от того на какой элемент мы нажали (можем узнать по содержанию в нём возможных классов) будет зависеть как мы найдём вопрос и удалим/добавим класс отображения
-    if (evt.target.classList.contains('accordeon-question')) {
-        evt.target.classList.toggle('expanded');
-    } else if (evt.target.classList.contains('accordeon-item')) {
-        const questionAsChild = evt.target.querySelector('.accordeon-question');
-        questionAsChild.classList.toggle('expanded');
-    } else if (evt.target.classList.contains('accordeon-answer')) {
+    if (evt.target.classList.contains('about-question')) {
+        evt.target.classList.toggle('circle');
+    } else if (evt.target.classList.contains('about-item')) {
+        const questionAsChild = evt.target.querySelector('.about-question');
+        questionAsChild.classList.toggle('circle');
+    } else if (evt.target.classList.contains('about-answer')) {
         evt.stopPropagation();
     }
 }
